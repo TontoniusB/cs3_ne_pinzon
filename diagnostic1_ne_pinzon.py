786e2f0b644cc3b9a654bf92414f26f616bf4118
Andrei Pinzon <andreipinzon24@gmail.com>
2026-08-12 16:09:17 +0800

    commit

--- diagnostic1_ne_pinzon.py
+++ diagnostic1_ne_pinzon.py
@@ -1,1 +1,18 @@
-#hello world testing1231git
+def calculate_checkout(cart_total, shipping_speed):
+    shipping_cost = 0
+    cart_total = float(cart_total)
+    cart_total = float("how much are you spending?")
+    if shipping_speed == "express":
+        shipping_cost = 20
+    elif shipping_speed == "overnight":
+        shipping_cost = 35
+    elif shipping_speed == "standard":
+        if cart_total >= 100:
+            shipping_cost = 0
+        elif cart_total < 100:
+            shipping_cost = 10
+    else:
+        print("You can't do that.")
+    return shipping_cost + cart_total
+
+print(calculate_checkout(10000, "standard"))
