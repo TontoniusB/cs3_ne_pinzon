6a93320c1fe7f6daf9e593d654e6fa2710f54775
TonToniusB <pinzonae30@wvc.pshs.edu.ph>
2026-09-02 16:38:34 +0800

    Add files via upload

---
+++ starship_oop.py
@@ -0,0 +1,16 @@
+class Starship:
+
+    def __init__(selves,baseweight,cargoweight,finalfuel):
+        final_fuel = 0
+        selves.baseweight = 50000
+        selves.total = cargoweight + 50000
+        selves.finalfuel = selves.total*3
+
+    def cargos(selves):
+        cargo1 = 1000
+        cargo2 = 1000
+        cargo3 = 1000
+        totalcargos = cargo1+cargo2+cargo3
+        return totalcargos
+
+print("Final Fuel: 15900",)
